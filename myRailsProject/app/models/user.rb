6a2ab461769766fb1058a1433b83ec0fed2ee75e
Paul <paul.demets@etu.u-bordeaux.fr>
2020-01-29 09:13:02 +0100

    user are good

--- myRailsProject/app/models/user.rb
+++ myRailsProject/app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         validates :first_name, presence: true
+         validates :last_name, presence: true
  
 end
